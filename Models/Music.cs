4ad5f9b5cb4aa9a87b4e837417318e7a9e5d4985
MonicaRochaa <nataly.army00@gmail.com>
2025-03-13 21:29:36 -0300

    Adionado arquivo para usar o TimeSpanConverter e o JsonConverter na pasta Musica.cs

--- Models/Music.cs
+++ Models/Music.cs
@@ -1,9 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace BTSCataloAPI.Models
 {
     public class Music
     {
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
+    [JsonConverter(typeof(TimeSpanConverter))] 
     public TimeSpan Duracao { get; set; }
     public string Album {get; set;} = string.Empty;
     //public Album Album {get; set; } 
