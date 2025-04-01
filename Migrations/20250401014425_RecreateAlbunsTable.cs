94298de162b3f2b8219c089d30357f0d55623f61
MonicaRochaa <nataly.army00@gmail.com>
2025-03-31 23:22:15 -0300

    Adicionado novas migrations para adicionar a tabela Album e Musica

---
+++ Migrations/20250401014425_RecreateAlbunsTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTSCataloAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecreateAlbunsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albuns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Artist = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albuns", x => x.Id);
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Albuns");
+        }
+    }
+}
