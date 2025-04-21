042dcdc649f5b7c049aeaea3b63b2e97bd3ccd9a
MonicaRochaa <nataly.army00@gmail.com>
2025-04-20 22:31:06 -0300

    retirada a dependia ao banco

--- Controllers/MusicController.cs
+++ Controllers/MusicController.cs
@@ -1,40 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
-using BTSCataloAPI.Models;
-using System.Collections.Generic;
-using BTSCataloAPI.Data;
 
 [Route("api/[controller]")]
 [ApiController]
 public class MusicController : ControllerBase
 {
-    
-    private readonly AppDbContext _context;
-
-    public MusicController(AppDbContext context)
-    {
-        _context = context;
-    }
-
     [HttpGet]
     public IActionResult GetMusics()
     {
-        var musics = _context.Musics.ToList();
-        return Ok(musics);
-    }
-
-
     
-    [HttpPost]
-    public ActionResult<Music> AddMusic([FromBody] Music novaMusic)
-    {
-        if (novaMusic == null )
-            {
-                return BadRequest("Música inválida.");
-            }
-
-    _context.Musics.Add(novaMusic);
-    _context.SaveChanges();
-    return CreatedAtAction(nameof(GetMusics), new { id = novaMusic.Id }, novaMusic);
-}
-
+        return Ok("Em breve: músicas vindas do Spotify!");
+    }
 }
